1c008754e092802aa20b8f8e88a124d930b28992
Dennis Buduev <dbuduev@gmail.com>
2016-02-23 20:19:10 +1300

    Fix database to use workers properly. Add supervisor.

--- code_samples/ch07/persistable_todo_cache/lib/todo/cache.ex
+++ code_samples/ch07/persistable_todo_cache/lib/todo/cache.ex
@@ -1,17 +1,17 @@
 defmodule Todo.Cache do
   use GenServer
 
-  def start do
+  def start_link do
 	IO.puts "Starting Todo.Cache"
-	GenServer.start(__MODULE__, nil)
+	GenServer.start_link(__MODULE__, nil, name: :todo_cache)
   end
 
-  def server_process(cache_pid, todo_list_name) do
-    GenServer.call(cache_pid, {:server_process, todo_list_name})
+  def server_process(todo_list_name) do
+    GenServer.call(:todo_cache, {:server_process, todo_list_name})
   end
 
   def init(_) do
-    Todo.Database.start("./persist/")
+    Todo.Database.start_link("./persist/")
     {:ok, HashDict.new}
   end
 
--- code_samples/ch07/persistable_todo_cache/lib/todo/database.ex
+++ code_samples/ch07/persistable_todo_cache/lib/todo/database.ex
@@ -2,40 +2,36 @@ defmodule Todo.Database do
   use GenServer
   @workers_num  3
   
-  def start(db_folder) do
+  def start_link(db_folder) do
 	IO.puts "Starting Todo.Database"	
-	GenServer.start(__MODULE__, db_folder, name: :database_server)
-  end
-
-  def store(key, data) do
-    GenServer.cast(:database_server, {:store, key, data})
-  end
-
-  def get(key) do
-    GenServer.call(:database_server, {:get, key})
+	GenServer.start_link(__MODULE__, db_folder, name: :database_server)
   end
 
   def init(db_folder) do
     File.mkdir_p(db_folder)
     state = 1..@workers_num |> Enum.reduce(HashDict.new, fn(i, acc) -> 
-		{:ok, worker_id} = Todo.DatabaseWorker.start(db_folder)
-		HashDict.put(acc, i, worker_id) end)
+		{:ok, worker_id} = Todo.DatabaseWorker.start_link(db_folder)
+		HashDict.put(acc, i - 1, worker_id) end)
 	IO.inspect state
 	{:ok, state}
   end
-
-  def handle_cast({:store, key, data}, workers) do
-	worker_id = get_worker(workers, key)
-	Todo.DatabaseWorker.store(worker_id, key, data)
-    {:noreply, workers}
+  
+  def store(key, data) do
+    get_worker(key) |> Todo.DatabaseWorker.store(key, data)
   end
 
-  def handle_call({:get, key}, _, workers) do
-	worker_id = get_worker(workers, key)
-    data = Todo.DatabaseWorker.get(worker_id, key)
-    {:reply, data, workers}
+  def get(key) do
+	get_worker(key) |> Todo.DatabaseWorker.get(key)	
+  end
+  
+  defp get_worker(key) do
+    GenServer.call(:database_server, {:get_worker, key})
   end
 
+  def handle_call({:get_worker, key}, _, workers) do
+    {:reply, get_worker(workers, key), workers}
+  end
+  
   # Needed for testing purposes
   def handle_info(:stop, state), do: {:stop, :normal, state}
   def handle_info(_, state), do: {:noreply, state}
